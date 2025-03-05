9dc0a0517669385436c58a6c9e70b9fa2902b35d
latifii <latifii68@gmail.com>
2025-03-05 18:05:08 +0330

    add pages

---
+++ app/layout.jsx
@@ -0,0 +1,31 @@
+import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+import Header from "./_components/Header";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: {
+    template: "%s / The Booking",
+    default: "Welcome / The Booking",
+  },
+  description: "Good Suite booking in world",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${josefin.className} min-h-screen bg-primary-950 text-primary-100 flex flex-col antialiased relative`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
