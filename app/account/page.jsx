4fff867604d6780b0b105cfcd1ada7bf381fdec5
latifii <latifii68@gmail.com>
2025-03-20 02:00:45 +0330

    done auth

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -1,7 +1,10 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Account",
 };
 
-export default function Page() {
-  return <div>Welcome Hamed Latifi</div>;
+export default async function Page() {
+  const session = await auth();
+  return <div>Welcome {session?.user?.name}</div>;
 }
